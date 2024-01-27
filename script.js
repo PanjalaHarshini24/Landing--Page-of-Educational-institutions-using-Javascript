0e205acd66b7232a43d3d7f2265c70180cd84278
PanjalaHarshini24 <149800841+PanjalaHarshini24@users.noreply.github.com>
2024-01-27 23:28:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// change navbar styles on scroll
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll',window.scrollY>0)
+})
+//show hide nav menu
+const menu=document.querySelector("nav_menu");
+const menuBtn=document.querySelector("#open-menu-btn");
+const closeBtn=document.querySelector("#close-menu-btn");
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+})
+const closeNav=()=>{
+    menu.style.display="none";
+    closeBtn.style.dispaly="none";
+    menuBtn.style.display="inline-block";
+}
+closeBtn.addEventListener('click',closeNav)
